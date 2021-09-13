78a4f58688dfd73aaceb9d93862228de641e50bf
Thibault Thomas <thibaulthomas@gmail.com>
2021-09-13 16:01:43 +0200

    Modèles

--- movietut/models.py
+++ movietut/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200, default='')
+    name_fr = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    id = models.IntegerField(primary_key=True)
+    poster = models.CharField(max_length=200, null=True)
+    adult = models.BooleanField()
+    overview = models.TextField()
+    release_date = models.DateField()
+    original_title = models.CharField(max_length=200)
+    original_language = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    genres = models.ManyToManyField(Genre)
+
+
