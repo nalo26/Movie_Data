831ac10694bcb8c91258c585ab9aec0d89d44417
Galding <Epulapp@EPULAPP2101.univ-lyon1.fr>
2021-09-16 10:10:27 +0200

    create homepage and carrousel for next released movies

--- movietut/urls.py
+++ movietut/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MovieListView.as_view(), name='index'),
+    path('', views.index, name='index'),
+    path('all-movies/', views.MovieListView.as_view(), name='all-movies'),
     path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
 ]
--- movietut/views.py
+++ movietut/views.py
@@ -1,6 +1,18 @@
+from datetime import time
 from django.views.generic import ListView, DetailView
 from .models import Movie
+from django.shortcuts import render
+from django.utils import timezone
 
+def index(request):
+    start_date = timezone.now()
+    end_date = start_date + timezone.timedelta(days=14)
+    movies_to_be_released = Movie.objects.filter(release_date__range=[start_date, end_date])
+    context = {
+        "movies_to_be_released": movies_to_be_released,
+        "movies": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 
+    }
+    return render(request, "movietut/index.html", context)
 
 class MovieListView(ListView):
     model = Movie
