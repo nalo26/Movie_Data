210cc9132dccc1338a951abe14d36fb50195fa0e
Émilie Vey <nath.v26@gmail.com>
2021-09-16 09:20:26 +0200

    updated db model

--- movietut/models.py
+++ movietut/models.py
@@ -9,10 +9,24 @@ class Genre(models.Model):
     def __str__(self):
         return self.name
 
+class Production_Country(models.Model):
+    iso = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Production_Company(models.Model):
+    id = models.IntegerField(primary_key=True)
+    logo = models.CharField(max_length=200, default=True)
+    name = models.CharField(max_length=200)
+    origin_country = models.CharField(max_length=200)
+
+class Spoken_Languages(models.Model):
+    iso = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=100)
 
 class Movie(models.Model):
     id = models.IntegerField(primary_key=True)
-    poster = models.CharField(max_length=200, null=True)
+    imdb_id = models.CharField(max_length=10, default="")
+    poster = models.CharField(max_length=200, default="")
     adult = models.BooleanField()
     overview = models.TextField()
     release_date = models.DateField()
@@ -22,7 +36,15 @@ class Movie(models.Model):
     popularity = models.FloatField()
     vote_count = models.IntegerField()
     vote_average = models.FloatField()
+    budget = models.IntegerField(default=0)
+    revenue = models.IntegerField(default=0)
+    runtime = models.IntegerField(default=0)
+    status = models.CharField(max_length=200, default="")
+    tagline = models.CharField(max_length=200, default="")
     genres = models.ManyToManyField(Genre)
+    production_companies = models.ManyToManyField(Production_Company)
+    production_countries = models.ManyToManyField(Production_Country)
+    spoken_languages = models.ManyToManyField(Spoken_Languages)
 
     def __str__(self):
         return f"{self.title} ({self.release_date.year})"
