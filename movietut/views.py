1abc2f79b8054711e015c3ed43068afef18b46e1
Galding <Epulapp@EPULAPP2101.univ-lyon1.fr>
2021-09-17 11:35:41 +0200

    add carousel for top rated movies and recommendations

--- movietut/views.py
+++ movietut/views.py
@@ -9,11 +9,21 @@ def index(request):
     end_date = start_date + timezone.timedelta(days=14)
     movies_to_be_released = Movie.objects.filter(release_date__range=[start_date + timedelta(days=1), end_date]).order_by("release_date")
     movies_recently_released= Movie.objects.filter(release_date__range=[start_date - timedelta(days=14), start_date]).order_by("-release_date")
+    movies_top_rated= Movie.objects.filter(popularity__gt=80.0).order_by("popularity")
+
     context = {
         "movies_to_be_released": movies_to_be_released,
         "movies_recently_released": movies_recently_released,
+        "movies_top_rated": movies_top_rated,
     }
+
+    if(request.user.is_authenticated):
+        context["movies_recommended"] = get_users_recommendations()
+            
     return render(request, "movietut/index.html", context)
+
+def get_users_recommendations():
+    return None
 
 class MovieListView(ListView):
     model = Movie
