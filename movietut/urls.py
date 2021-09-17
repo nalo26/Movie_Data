fab8af9a1bd43d12e8b792ad7415e0bf3c3d15a4
allan des courtils <Epulapp@EPULAPP2103.univ-lyon1.fr>
2021-09-17 11:53:10 +0200

    add view to login user

--- movietut/urls.py
+++ movietut/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.MovieListView.as_view(), name='index'),
     path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
+    path('login/', views.login, name='login'),
 ]
+
+
