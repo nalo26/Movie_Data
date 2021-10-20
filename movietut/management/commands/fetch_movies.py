766b2002953c7e2f1ad9122e14171aa38abc23f4
Émilie Vey <nath.v26@gmail.com>
2021-10-20 08:41:43 +0200

    filled up database

--- movietut/management/commands/fetch_movies.py
+++ movietut/management/commands/fetch_movies.py
@@ -7,47 +7,49 @@ from tqdm import tqdm
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        # MIN_YEAR = 1885
-        MIN_YEAR = 2021
+        MIN_YEAR = 1885
         MAX_YEAR = 2021
 
         API_URI = "https://api.themoviedb.org/3"
         API_KEY = "e7e2852e5b4d9cc2fccd5fb9858411d9"
         current_id = -1
-        try:
-            for year in range(MIN_YEAR, MAX_YEAR + 1):
-                print(year)
-                data = rq.get(f"{API_URI}/discover/movie?api_key={API_KEY}&primary_release_year={year}").json()
-                pages = int(data['total_pages'])
+        for year in range(MIN_YEAR, MAX_YEAR + 1):
+            print(year)
+            data = rq.get(f"{API_URI}/discover/movie?api_key={API_KEY}&primary_release_year={year}").json()
+            pages = int(data['total_pages'])
 
-                for page in tqdm(range(1, pages + 1)):
-                    movies = rq.get(f"{API_URI}/discover/movie?api_key={API_KEY}&primary_release_year={year}&page={page}").json()
+            for page in tqdm(range(1, pages + 1)):
+                movies = rq.get(f"{API_URI}/discover/movie?api_key={API_KEY}&primary_release_year={year}&page={page}").json()
 
-                    for jmovie in movies['results']:
-                        current_id = jmovie.get('id')
+                for jmovie in movies['results']:
+                    current_id = jmovie.get('id')
+
+                    try:
                         jmovie = rq.get(f"{API_URI}/movie/{jmovie.get('id')}?api_key={API_KEY}").json()
-                        movie, _ = Movie.objects.get_or_create(
+                        movie, _ = Movie.objects.update_or_create(
                             id = jmovie.get('id'),
-                            poster = jmovie.get('poster_path', '') if jmovie.get('poster_path') is not None else '',
-                            adult = jmovie.get('adult', False),
-                            overview = jmovie.get('overview') if jmovie.get('overview') is not None else '',
-                            release_date = jmovie.get('release_date', ''),
-                            original_title = jmovie.get('original_title', ''),
-                            original_language = jmovie.get('original_language', ''),
-                            title = jmovie.get('title', ''),
-                            popularity = jmovie.get('popularity', 0),
-                            vote_count = jmovie.get('vote_count', 0),
-                            vote_average = jmovie.get('vote_average', 0),
-                            imdb_id = jmovie.get('imdb_id') if jmovie.get('imdb_id') is not None else '',
-                            budget = jmovie.get('budget', 0),
-                            revenue = jmovie.get('revenue', 0),
-                            runtime = jmovie.get('runtime') if jmovie.get('runtime') is not None else 0,
-                            status = jmovie.get('status', ''),
-                            tagline = jmovie.get('tagline') if jmovie.get('tagline') is not None else '',
+                            defaults = {
+                                "poster": jmovie.get('poster_path', '') if jmovie.get('poster_path') is not None else '',
+                                "adult": jmovie.get('adult', False),
+                                "overview": jmovie.get('overview') if jmovie.get('overview') is not None else '',
+                                "release_date": jmovie.get('release_date', ''),
+                                "original_title": jmovie.get('original_title', '')[:200],
+                                "original_language": jmovie.get('original_language', ''),
+                                "title": jmovie.get('title', '')[:200],
+                                "popularity": jmovie.get('popularity', 0),
+                                "vote_count": jmovie.get('vote_count', 0),
+                                "vote_average": jmovie.get('vote_average', 0),
+                                "imdb_id": jmovie.get('imdb_id') if jmovie.get('imdb_id') is not None else '',
+                                "budget": jmovie.get('budget', 0),
+                                "revenue": jmovie.get('revenue', 0),
+                                "runtime": jmovie.get('runtime') if jmovie.get('runtime') is not None else 0,
+                                "status": jmovie.get('status', ''),
+                                "tagline": jmovie.get('tagline') if jmovie.get('tagline') is not None else '',
+                            }
                         )
 
-                        for jgenre in jmovie.get('genre_ids', []):
-                            movie.genres.add(jgenre)
+                        for jgenre in jmovie.get('genres', []):
+                            movie.genres.add(jgenre.get('id'))
 
                         for prod_count in jmovie.get('production_countries', []):
                             obj, _ = Production_Country.objects.get_or_create(
@@ -57,11 +59,13 @@ class Command(BaseCommand):
                             movie.production_countries.add(obj)
 
                         for prod_comp in jmovie.get('production_companies', []):
-                            obj, _ = Production_Company.objects.get_or_create(
+                            obj, _ = Production_Company.objects.update_or_create(
                                 id = prod_comp.get('id'),
-                                logo = prod_comp.get('logo_path') if prod_comp.get('logo_path') is not None else "",
-                                name = prod_comp.get('name', ''),
-                                origin_country = prod_comp.get('origin_country', '')
+                                defaults = {
+                                    "logo": prod_comp.get('logo_path') if prod_comp.get('logo_path') is not None else "",
+                                    "name": prod_comp.get('name', ''),
+                                    "origin_country": prod_comp.get('origin_country') if prod_comp.get('origin_country') is not None else ""
+                                }
                             )
                             movie.production_companies.add(obj)
 
@@ -71,8 +75,11 @@ class Command(BaseCommand):
                                 name = lang.get('name')
                             )
                             movie.spoken_languages.add(obj)
-                        
+
                         movie.save()
-        except Exception as e:
-            print(e)
-            print(current_id)
+
+                    except Exception as e:
+                        if "format de date de la valeur" in str(e): continue
+                        print(e)
+                        print(current_id)
+                        # print(jmovie)
