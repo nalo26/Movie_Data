e74e5b6237d3c3ac369762430abaf788d0014c28
Émilie Vey <nath.v26@gmail.com>
2022-02-10 09:41:10 +0100

    final commit

--- movietut/apps.py
+++ movietut/apps.py
@@ -12,7 +12,7 @@ class MovietutConfig(AppConfig):
         if os.environ.get('RUN_MAIN', None) != 'true':
             from .refresh_movies import background_refresh_movie
             from .clusterImpl import init
-            # refreshMovieThread = Thread(target=background_refresh_movie, args=(86400,), daemon=True)
-            # refreshMovieThread.start()
-            # createClusterThread = Thread(target=init, daemon=True)
-            # createClusterThread.start()   
+            refreshMovieThread = Thread(target=background_refresh_movie, args=(86400,), daemon=True)
+            refreshMovieThread.start()
+            createClusterThread = Thread(target=init, daemon=True)
+            createClusterThread.start()   
